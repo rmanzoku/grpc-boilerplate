9870ff77c464aa8209ddeb95b9e80ea922067491
Ryo Manzoku <manzoku.ryo@gmail.com>
2021-01-15 14:48:54 +0900

    change handler

--- cmd/lambdaserver/main.go
+++ cmd/lambdaserver/main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
-	"github.com/rmanzoku/grpc-boilerplate/cmd/lambdaserver/grpcgwmux"
 	"github.com/rmanzoku/grpc-boilerplate/feature/ping"
 )
 
-var adapter *grpcgwmux.GrpcGWMuxLambda
+var adapter *httpadapter.HandlerAdapter
 
 func registerServices(mux *runtime.ServeMux) (err error) {
 	ctx := context.Background()
@@ -28,7 +30,16 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	adapter = grpcgwmux.New(mux)
+	adapter = httpadapter.New(accessLogMiddleware(mux))
+}
+
+func accessLogMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		method := r.Method
+		path := r.URL.Path
+		fmt.Printf("method:%s\tpath:%s\n", method, path)
+		h.ServeHTTP(w, r)
+	})
 }
 
 func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
