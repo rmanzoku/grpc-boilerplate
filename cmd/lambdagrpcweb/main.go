04f8d53c90dacb1699e4cd5bb119154381e8eb92
Ryo Manzoku <manzoku.ryo@gmail.com>
2021-01-19 00:13:05 +0900

    grpc web works

--- cmd/lambdagrpcweb/main.go
+++ cmd/lambdagrpcweb/main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_logger "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
 	"github.com/improbable-eng/grpc-web/go/grpcweb"
 	"github.com/rmanzoku/grpc-boilerplate/feature/ping"
 	ping_service "github.com/rmanzoku/grpc-boilerplate/service/ping"
+	"go.uber.org/zap"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
 )
 
@@ -21,19 +27,39 @@ func registerServices(s *grpc.Server) {
 }
 
 func init() {
-	s := grpc.NewServer()
-	registerServices(s)
-	wrappedGrpc := grpcweb.WrapServer(s)
-	adapter = httpadapter.New(accessLogMiddleware(wrappedGrpc))
-}
+	// Logging
+	logger := zap.NewExample()
+	grpc_logger.ReplaceGrpcLoggerV2(logger)
 
-func accessLogMiddleware(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		method := r.Method
-		path := r.URL.Path
-		fmt.Printf("method:%s\tpath:%s\n", method, path)
-		h.ServeHTTP(w, r)
-	})
+	// Base grpc server
+	grpcServer := grpc.NewServer(grpc_middleware.WithUnaryServerChain(
+		grpc_ctxtags.UnaryServerInterceptor(),
+		grpc_logger.UnaryServerInterceptor(logger),
+		grpc_recovery.UnaryServerInterceptor(),
+	))
+	registerServices(grpcServer)
+
+	// Wrapping grpc-web server
+	grpcWebServer := grpcweb.WrapServer(grpcServer,
+		grpcweb.WithOriginFunc(func(origin string) bool { return true }),
+	)
+
+	// Wrapping http2 for standalone
+	httpServer := &http.Server{
+		Handler: h2c.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			grpcWebServer.ServeHTTP(w, r)
+
+			// for cors preflight
+			// grpcweb package use https://github.com/rs/cors/blob/master/cors.go#L202
+			// cors package doesn't exec WriteHeader, it is needed lambda proxy
+			if grpcWebServer.IsAcceptableGrpcCorsRequest(r) {
+				w.WriteHeader(http.StatusNoContent)
+			}
+		}), &http2.Server{}),
+	}
+
+	// Lambda adapter
+	adapter = httpadapter.New(httpServer.Handler)
 }
 
 func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
--- go.mod
+++ go.mod
@@ -6,10 +6,13 @@ require (
 	github.com/aws/aws-lambda-go v1.22.0
 	github.com/awslabs/aws-lambda-go-api-proxy v0.9.0
 	github.com/desertbit/timer v0.0.0-20180107155436-c41aec40b27f // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.2.2
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.1.0
-	github.com/improbable-eng/grpc-web v0.13.0
+	github.com/improbable-eng/grpc-web v0.13.1-0.20210105121553-b608386bfb82
 	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
 	github.com/rs/cors v1.7.0 // indirect
+	go.uber.org/zap v1.16.0
+	golang.org/x/net v0.0.0-20200822124328-c89045814202
 	google.golang.org/genproto v0.0.0-20210106152847-07624b53cd92
 	google.golang.org/grpc v1.34.0
 	google.golang.org/protobuf v1.25.0
